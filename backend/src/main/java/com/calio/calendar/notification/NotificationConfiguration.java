6447ea21c1064a7997197e9d65861c17c1afc031
junhaa <wooha34567@gmail.com>
2026-09-08 17:57:04 +0900

    test(notification): cover APNs message and gateway contracts

--- backend/src/main/java/com/calio/calendar/notification/NotificationConfiguration.java
+++ backend/src/main/java/com/calio/calendar/notification/NotificationConfiguration.java
@@ -1,17 +1,10 @@
 package com.calio.calendar.notification;
 
 import com.calio.calendar.notification.client.ApnsProperties;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableConfigurationProperties(ApnsProperties.class)
 public class NotificationConfiguration {
-
-    @Bean
-    ObjectMapper objectMapper() {
-        return new ObjectMapper();
-    }
 }
