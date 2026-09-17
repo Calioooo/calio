d766ee93b5f3aebbc3458c62d4d8fe8c7fb183ad
jinhahakim <kjh03124@naver.com>
2026-09-18 02:15:53 +0900

    feat(ios): add vote data models

---
+++ frontend/Calio/Calio/Services/DTO/Vote/VoteDTO.swift
@@ -0,0 +1,66 @@
+import Foundation
+
+struct CreateVoteRoomRequestDTO: Encodable {
+    let name: String
+    let candidateEndDate: String
+}
+
+struct VoteRoomResponseDTO: Decodable {
+    let publicId: UUID
+    let name: String
+    let candidateStartDate: String
+    let candidateEndDate: String
+}
+
+struct VoteDateResultResponseDTO: Decodable {
+    let date: String
+    let unavailableCount: Int
+    let unavailableNicknames: [String]
+}
+
+struct VoteResultResponseDTO: Decodable {
+    let publicId: UUID
+    let name: String
+    let candidateStartDate: String
+    let candidateEndDate: String
+    let dates: [VoteDateResultResponseDTO]
+    let submittedNicknames: [String]
+}
+
+enum VoteParticipantStatusDTO: String, Decodable {
+    case registered = "REGISTERED"
+    case submitted = "SUBMITTED"
+}
+
+struct CreateVoteParticipantRequestDTO: Encodable {
+    let nickname: String
+    let password: String?
+}
+
+struct VoteParticipantResponseDTO: Decodable {
+    let nickname: String
+    let status: VoteParticipantStatusDTO
+}
+
+struct LookupVoteParticipantSelectionRequestDTO: Encodable {
+    let nickname: String
+    let password: String?
+}
+
+struct VoteParticipantSelectionResponseDTO: Decodable {
+    let nickname: String
+    let status: VoteParticipantStatusDTO
+    let unavailableDates: [String]
+}
+
+struct SubmitVoteRequestDTO: Encodable {
+    let nickname: String
+    let password: String?
+    let unavailableDates: [String]
+}
+
+struct VoteSubmissionResponseDTO: Decodable {
+    let nickname: String
+    let status: VoteParticipantStatusDTO
+    let unavailableDates: [String]
+}
